64c830c6863d64ac808e90379c59dd19a6cf7b24
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-06-01 22:59:05 -0300

    finished with weather chatbot app

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request # this is request Class from Flask
+import requests # This is a third-party Python library that provides a simple and elegant way to make HTTP requests.
 
 app = Flask(__name__)
 
@@ -17,19 +18,19 @@ bot = ChatBot("chatbot", read_only=False,
 # trainer = ChatterBotCorpusTrainer(bot)
 # trainer.train("chatterbot.corpus.english")
 
-trainer = ListTrainer(bot)
-trainer.train([
-    "hello",
-    "hello!",
-    "How's it going?",
-    "not bad!",
-    "How can I lose weight?",
-    "You've got to work out mate!",
-    "Any suggestion?",
-    "Yes, 1- work out 2- stop eating everything 3- follow a keto diet",
-    "Other suggestions?",
-    "Please visit this link for more info: https://example.com"
-])
+# trainer = ListTrainer(bot)
+# trainer.train([
+#     "hello",
+#     "hello!",
+#     "How's it going?",
+#     "not bad!",
+#     "How can I lose weight?",
+#     "You've got to work out mate!",
+#     "Any suggestion?",
+#     "Yes, 1- work out 2- stop eating everything 3- follow a keto diet",
+#     "Other suggestions?",
+#     "Please visit this link for more info: https://example.com"
+# ])
 
 # ROUTING WEB PAGES
 @app.route("/")
@@ -38,8 +39,16 @@ def main():
 
 @app.route("/get")
 def get_chatbot_response():
+
+    # GET request from Flask
     userText = request.args.get('userMessage')
-    return str(bot.get_response(userText))
+
+    # GET request from module requests
+    rawData = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+ userText +"&appid=064f1afb235afd36530ee0e6825fc231")
+    result = rawData.json()
+
+    # return str(bot.get_response(userText))
+    return result
 
 
 
